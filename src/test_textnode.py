9fd70909110fb46a8b8ea63f3b3f709106a4eef7
Vergiet <djhhoornweg@hotmail.com>
2025-06-02 10:32:56 +0200

    add initial tests

---
+++ src/test_textnode.py
@@ -0,0 +1,24 @@
+﻿import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.Bold, 'boot.dev')
+        node2 = TextNode("This is a text node", TextType.Bold, 'boot.dev')
+        self.assertEqual(node, node2)
+
+    def test_ne(self):
+        node = TextNode("This is also a text node", TextType.Bold, 'boot.dev')
+        node2 = TextNode("This is a text node", TextType.Bold, 'boot.dev')
+        self.assertNotEqual(node, node2)
+
+    def test_ne_2(self):
+        node = TextNode("This is a text node", TextType.Link, 'boot.dev')
+        node2 = TextNode("This is a text node", TextType.Bold, 'boot.dev')
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
