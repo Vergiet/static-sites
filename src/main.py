d7633dc2aedc67281232535d73162ba943942081
Vergiet <djhhoornweg@hotmail.com>
2025-06-02 10:09:46 +0200

    add initial repo structure

---
+++ src/main.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env python
+from textnode import TextNode, TextType
+
+print("hello world")
+
+
+def main():
+    print(TextNode('This is some anchor text', (TextType.Link), 'https://www.boot.dev'))
+
+
+
+
+if __name__=="__main__":
+    main()
