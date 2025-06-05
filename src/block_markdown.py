a8a660e989d49b4d8a9b056fdb24f5f2279d3b7e
Vergiet <djhhoornweg@hotmail.com>
2025-06-05 09:03:54 +0200

    add markdown to block

---
+++ src/block_markdown.py
@@ -0,0 +1,16 @@
+﻿
+
+def markdown_to_blocks(raw_markdown):
+    md_blocks = []
+    new_block = []
+    for line in raw_markdown.splitlines():
+        if line == "":
+            if len(new_block) > 0:
+                md_blocks.append('\n'.join(new_block))
+            new_block = []
+        else:
+            new_block.append(line)
+    md_blocks.append('\n'.join(new_block))
+    return md_blocks
+
+
