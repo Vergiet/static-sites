dae1af316c6ac60d74ee49e7b132cffe5e3e6c11
Vergiet <djhhoornweg@hotmail.com>
2025-06-07 07:40:24 +0200

    rm whitespace

--- src/text_to_textnodes.py
+++ src/text_to_textnodes.py
@@ -6,8 +6,4 @@ def text_to_textnodes(text):
     nodes = [TextNode(text, TextType.TEXT)]
     new_nodes = []
     new_nodes.extend(split_nodes_link(split_nodes_image(split_nodes_delimiter(split_nodes_delimiter(split_nodes_delimiter(nodes, "**", TextType.BOLD), "_", TextType.ITALIC), "`", TextType.CODE))))
-
-
-
-
     return new_nodes
